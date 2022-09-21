3e8fe3abb24b086356e6017dbf15e81401701c79
Crhystian <cdavid2127@gmail.com>
2022-09-21 19:43:24 +0200

    Json Implementation, Model, themes and configuration

---
+++ app/src/main/java/co/utp/aves/presentation/bird/model/Ave.kt
@@ -0,0 +1,16 @@
+package co.utp.aves.presentation.bird.model
+
+data class Ave(
+    val Alimentos: List<String>,
+    val Codigo: String,
+    val Distribucion: String,
+    val Familia: String,
+    val Imagen_Ave: String,
+    val Nombre_Cientifico: String,
+    val Nombre_Comun: String,
+    val Nombre_Ingles: String,
+    val Orden: String,
+    val Sonidos: List<String>,
+    val Tamano: String,
+    val Vocalizacion: String
+)
---
+++ app/src/main/java/co/utp/aves/presentation/bird/model/JsonAves.kt
@@ -0,0 +1,5 @@
+package co.utp.aves.presentation.bird.model
+
+data class JsonAves(
+    val Aves: List<Ave>
+)
