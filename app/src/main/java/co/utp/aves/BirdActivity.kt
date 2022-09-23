cf6b716ca970ef3907a8a39a1bc424e30ef57e4a
Cristian David Soto Ramirez <cdavid2127@gmail.com>
2022-09-23 06:20:49 -0500

    Ui Configuration RootActivity and BirdActivity

--- app/src/main/java/co/utp/aves/BirdActivity.kt
+++ app/src/main/java/co/utp/aves/BirdActivity.kt
@@ -2,24 +2,62 @@ package co.utp.aves
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.content.ContextCompat
 import co.utp.aves.base.BaseActivity
 import co.utp.aves.databinding.ActivityBirdBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class BirdActivity : BaseActivity<ActivityBirdBinding>(){
-
 
     override fun inflateView(inflater: LayoutInflater)=
         ActivityBirdBinding.inflate(inflater)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        bottomNavigation()
+        binding.toolbarMainActivity.inflateMenu(R.menu.toolbar_quit_menu)
+        actionQuitToolbar()
     }
 
     private fun bottomNavigation(){
-        binding.bottomNavMenu.setOnItemReselectedListener { menu ->
-            when (menu.title) {
 
+
+    }
+
+    override fun setToolbarTitle(title: String) {
+        binding.toolbarMainActivity.title = "Aves de Pereira"
+    }
+
+    override fun setToolbarStyle(titleColor: Int?, backgroundColor: Int?) {
+        binding.apply {
+            titleColor?.let { color ->
+                toolbarMainActivity.setTitleTextColor(
+                    ContextCompat.getColor(
+                        this@BirdActivity,
+                        color
+                    )
+                )
+            }
+            backgroundColor?.let { color ->
+                toolbarMainActivity.setBackgroundColor(
+                    ContextCompat.getColor(
+                        this@BirdActivity,
+                        color
+                    )
+                )
+            }
+        }
+    }
+
+    private fun actionQuitToolbar(){
+        binding.toolbarMainActivity.setOnMenuItemClickListener{
+            when (it.itemId) {
+                R.id.action_quit -> {
+                    finish()
+                    true
+                }
+                else -> false
             }
         }
     }
---
+++ app/src/main/java/co/utp/aves/RootActivity.kt
@@ -0,0 +1,24 @@
+package co.utp.aves
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import co.utp.aves.base.BaseActivity
+import co.utp.aves.databinding.ActivityRootBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RootActivity : BaseActivity<ActivityRootBinding>() {
+
+    override fun inflateView(inflater: LayoutInflater) = ActivityRootBinding.inflate(inflater)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun setUI() {
+        startActivity(Intent(this@RootActivity, BirdActivity::class.java))
+    }
+}
